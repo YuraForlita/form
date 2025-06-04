5423d791ee1c1754e6d7fe45813c7d832e981e0c
YuraForlita <forlitaura@gmail.com>
2025-06-04 13:54:48 +0300

    ,

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const products = {
         description: "Міцна дерев’яна лавка з натурального дерева.",
         images: [
             "https://images.prom.ua/3247175368_derevyana-lavka-kim.jpg",
-            "https://images.prom.ua/3247175368_derevyana-lavka-kim.jpg"
+            "https://images.prom.ua/3247220200_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247224895_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247171662_derevyana-lavka-kim.jpg"
         ]
     },
     lavka2: {
@@ -18,7 +20,32 @@ const products = {
         price: "1 350 грн",
         description: "Сучасна лавка з покриттям для вулиці.",
         images: [
-            "https://images.prom.ua/3247220200_derevyana-lavka-kim.jpg"
+            "https://images.prom.ua/3247175368_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247220200_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247224895_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247171662_derevyana-lavka-kim.jpg"
+        ]
+    },
+    lavka3: {
+        name: "Лавка номер 3",
+        price: "5 350 грн",
+        description: "Афігена лавка для вулиці.",
+        images: [
+            "https://images.prom.ua/3247175368_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247220200_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247224895_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247171662_derevyana-lavka-kim.jpg"
+        ]
+    },
+    lavka4: {
+        name: "Лавка номер 4",
+        price: "5 350 грн",
+        description: "лавка на якій можна файно посидіти, хаха.",
+        images: [
+            "https://images.prom.ua/3247175368_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247220200_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247224895_derevyana-lavka-kim.jpg",
+            "https://images.prom.ua/3247171662_derevyana-lavka-kim.jpg"
         ]
     }
 };
